9660e65ad708a76aa693cbe2d82ab66638e2cefa
Marcos Vinicius <oficialviniciussantana@gmail.com>
2026-04-16 18:16:41 -0300

    Correções

--- PDV_GESTÃO/API/API_PDV-main/src/database/databaseLogin/index.js
+++ PDV_GESTÃO/API/API_PDV-main/src/database/databaseLogin/index.js
@@ -5,7 +5,7 @@ const connection = mysql.createPool({
     host:process.env.DB_HOST,
     user:process.env.DB_USERNAME,
     password:process.env.DB_PASSWORD,
-    database:'dbgestaoficha',
+    database:'gestaodatabase',
     connectionLimit:2
 })
 
